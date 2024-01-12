59628bdbdadcad22b847b977183ebe415550f06b
SDI-sodya <saienko.dan06sdi@gmail.com>
2024-01-12 21:58:47 +0200

    Add old components in folder src

---
+++ src/components/MainContent/index.jsx
@@ -0,0 +1,10 @@
+function MainContent() {
+	return (
+		<main>
+			<h2>Content heading</h2>
+			<p>Lorem asdadasfasdad</p>
+		</main>
+	);
+}
+
+export default MainContent;
---
+++ src/components/Phone/index.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+class Phone extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			phoneState: "off",
+		};
+	}
+
+	render() {
+		const { color, price } = this.props;
+		const { phoneState } = this.state;
+		return (
+			<div>
+				<p>Phone is {color}</p>
+				<p>Phone price is {price}</p>
+				<p>Phone is currently {phoneState}</p>
+				<button>Switch on</button>
+			</div>
+		);
+	}
+}
+
+export default Phone;
