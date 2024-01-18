0e925fe80e54cb1d9b8babd51ac7d3dfd7bebec8
SDI-sodya <saienko.dan06sdi@gmail.com>
2024-01-18 20:17:00 +0200

    Add component about phones

--- src/components/Phone/index.jsx
+++ src/components/Phone/index.jsx
@@ -1,25 +1,93 @@
 import React from "react";
+import "./style.css";
 
 class Phone extends React.Component {
 	constructor(props) {
 		super(props);
+		// стан існує тільки у класових компонентів
 		this.state = {
-			phoneState: "off",
+			isOn: false,
 		};
+
+		// прикрутили this
+		this.handleToggleOn = this.handleToggleOn.bind(this);
 	}
 
+	handleToggleOn() {
+		// setState - метод який приймає об'єкт і зливає його з поточним станом
+		// після чого реакт робить повторний рендер
+		const stateChanges = {
+			isOn: true,
+		};
+		this.setState(stateChanges);
+		/*
+      доробити телефону кнопку вимикання, 
+      при якій його стан має змінитися на off
+    */
+	}
+
+	// this береться автоматично
+	handleToggleOff = () => {
+		this.setState({
+			isOn: false,
+		});
+	};
+
+	handleToggle = () => {
+		const { isOn } = this.state;
+		this.setState({
+			// isOn: !this.state.isOn
+			isOn: !isOn,
+		});
+	};
+
 	render() {
-		const { color, price } = this.props;
-		const { phoneState } = this.state;
+		const {
+			phone: { color, price, isFavorite, id },
+			toggleFavorite,
+			addToCart,
+		} = this.props;
+		const { isOn } = this.state;
+
+		const phoneStatus = isOn ? <PhoneOnText /> : <PhoneOffText />;
+
+		const containerClass = `phoneContainer ${isOn ? "on" : "off"}`;
+
 		return (
-			<div>
+			<div className={containerClass}>
 				<p>Phone is {color}</p>
 				<p>Phone price is {price}</p>
-				<p>Phone is currently {phoneState}</p>
-				<button>Switch on</button>
+				{phoneStatus}
+				{isFavorite && <p>Phone is favorite</p>}
+				<button onClick={this.handleToggle}>
+					Switch {isOn ? "off" : "on"}
+				</button>
+				{/* <button onClick={isOn ? this.handleToggleOff : this.handleToggleOn}>
+          Switch {isOn ? 'off' : 'on'} v2
+        </button> */}
+				<button
+					onClick={() => {
+						toggleFavorite(id);
+					}}
+				>
+					{isFavorite ? "Remove from" : "Make"} favorite
+				</button>
+				<button onClick={() => addToCart(id)}>Add to cart</button>
 			</div>
 		);
 	}
+}
+
+/*
+  умовний рендерінг полягає у відмальуванні певних компонентів / елементів / тексту
+  за певної умови
+*/
+function PhoneOnText() {
+	return <p>Phone is currently on</p>;
+}
+
+function PhoneOffText() {
+	return <p>Phone is currently off</p>;
 }
 
 export default Phone;
---
+++ src/components/PhoneList/index.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import Phone from "../Phone";
+
+class PhoneList extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		const {
+			phones,
+			toggleFavorite,
+			addToCart,
+			isDirectSortOrder,
+			toggleSortOrder,
+		} = this.props;
+
+		// const elems = (
+		//   <>
+		//     <Phone color="black" price={15999.99} />
+		//     <button>sdsadsa</button>
+		//   </>
+		// );
+
+		// ви можете зробити мапом масиви реакт елементів / компонентів
+		// і відрендерити їх одразу
+		// Верхньому елементу / компоненту який ви повертаєте з мапи
+		// НЕОБХІДНО вказати унікальний для цього списка службовий проп
+		// key
+		const phonesComponents = phones.map((phone) => (
+			<Phone
+				key={phone.id}
+				phone={phone}
+				toggleFavorite={toggleFavorite}
+				addToCart={addToCart}
+			/>
+		));
+
+		// тут вже можна такий самий набір ключів як і у phonesComponents
+		// const phonesComponents2 = phones.map((phone) => (
+		//   <Phone key={phone.id} color={phone.color} price={phone.price} />
+		// ));
+
+		return (
+			<>
+				<button onClick={toggleSortOrder}>Reverse sort order</button>
+				<p>
+					Current sort order is: {isDirectSortOrder ? "direct" : "reversed"}
+				</p>
+				<ul>{phonesComponents}</ul>
+			</>
+		);
+	}
+}
+
+export default PhoneList;
