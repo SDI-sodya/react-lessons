4f4c112b2878d9cc8bcec4bb0e7bc98ecf554b8d
SDI-sodya <saienko.dan06sdi@gmail.com>
2024-01-18 20:17:57 +0200

    Add LoginForm component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import LoginForm from "./components/LoginForm";
+// import { format, addSeconds, add } from "date-fns";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class StopWatch extends Component {
+	constructor(props) {
+		super(props);
+
+		// this.state = {
+		// 	date: new Date(0, 0, 0, 0, 0, 0),
+		// };
+	}
+
+	// start = () => {
+	// 	if (!this.intervalId) {
+	// 		this.intervalId = setInterval(this.tick, 1000);
+	// 	}
+	// };
+
+	// stop = () => {
+	// 	clearInterval(this.intervalId);
+	// 	this.intervalId = null;
+	// };
+
+	// tick = () => {
+	// 	const { date } = this.state;
+
+	// 	// const newDate = addSeconds(date, 1);
+
+	// 	const newDate = add(date, { seconds: 1 });
+
+	// 	this.setState({
+	// 		date: newDate,
+	// 	});
+	// };
+
+	render() {
+		// const { date } = this.state;
+
+		// const timeText = format(date, "HH:mm:ss");
+
+		return (
+			<div>
+				<LoginForm />
+				{/* <p>{timeText}</p>
+				<button onClick={this.start}>Start</button>
+				<button onClick={this.stop}>Stop</button> */}
+			</div>
+		);
+	}
 }
 
-export default App;
+export default StopWatch;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,57 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+/*
+  Компонентів існує 2 види
+    класові
+    функціональні
+*/
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// класовий компонент
+// має успадковуватися від React.Component
+// class HeadingClass extends React.Component {
+//   // має мати метод render
+//   render() {
+//     // має повертати реакт елемент / компонент
+//     return <h1>Some heading class</h1>;
+//   }
+// }
+
+// // функціональний компонент
+// function HeadingFunction() {
+//   // має повертати реакт елемент / компонент
+//   return <h1>Some heading func</h1>;
+// }
+
+// створіть компонент який буде відмальовувати параграф тексту
+
+// class TextClassSolution extends React.Component {
+//   render() {
+
+//     return <p>text class</p>;
+//   }
+// }
+
+// function TextFunctionSolution () {
+//   return <p>text func</p>;
+// }
+
+/*
+  реакт не показує булеві значення, null, undefined, Symbol, ""
+  реакт кидається помилкою якщо малювати звичайний об'єкт
+  якщо малювати масив то все залежить від вмісту массиву
+  усе інше реакт малює нормально
+*/
+
+// відмальовування компоненту
+// const headerComponent = React.createElement(HeadingClass);
+// const headerComponent = <HeadingClass />;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// Фрагмент - спеціальний елемент який не залишає по собі вермстки у підсумку
+// використовувати якщо треба порендирити декілька елементівв без видимого контейнеру
+// ! не передавати атрибути / пропси
+// <></> === <React.Fragment></React.Fragment> === React.createElement(React.Fragment)
+root.render(<App />);
