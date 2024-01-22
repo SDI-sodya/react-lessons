e1bfaea4071ec68c1bd2f48b54b14d755c13e7ac
SDI-sodya <saienko.dan06sdi@gmail.com>
2024-01-22 19:51:02 +0200

    Show users

--- src/components/UserLoader/index.jsx
+++ src/components/UserLoader/index.jsx
@@ -11,6 +11,32 @@ class UserLoader extends Component {
 		};
 	}
 
+	componentDidMount() {
+		this.setState({
+			isLoading: true,
+		});
+
+		fetch("https://randomuser.me/api/?results=20&seed=test&page=1")
+			.then((res) => res.json())
+			.then((data) => {
+				// console.log(data);
+
+				this.setState({
+					users: data.results,
+				});
+			})
+			.catch((err) => {
+				this.setState({
+					isError: true,
+				});
+			})
+			.finally(() => {
+				this.setState({
+					isLoading: false,
+				});
+			});
+	}
+
 	render() {
 		const { users, isError, isLoading } = this.state;
 
@@ -22,13 +48,18 @@ class UserLoader extends Component {
 			return <h1>ERROR HAPPENED</h1>;
 		}
 
-		const userCards = users.map((user) => (
-			<article>
-				<p>{user}</p>
-			</article>
-		));
+		const userCards = users.map(
+			({ name: { first, last }, picture: { thumbnail: src }, login: uuid }) => (
+				<article>
+					<h2>
+						{first} {last}
+					</h2>
+					<img src={src} alt={`${first} ${last}`} />
+				</article>
+			)
+		);
 
-		return <div></div>;
+		return <div>{userCards}</div>;
 	}
 }
 
