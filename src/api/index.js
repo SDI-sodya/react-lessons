8e5de0090c9f26ee4d5c44ba1f944c7e247ae782
SDI-sodya <saienko.dan06sdi@gmail.com>
2024-01-23 17:38:42 +0200

    Add API example

---
+++ src/api/index.js
@@ -0,0 +1,9 @@
+export async function getUsers(currentPage) {
+	const res = await fetch(
+		`https://randomuser.me/api/?results=20&seed=test&page=${currentPage}`
+	);
+
+	const { results } = await res.json();
+
+	return results;
+}
